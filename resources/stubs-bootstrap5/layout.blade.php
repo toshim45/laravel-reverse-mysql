1a70e1a2144ddcfb10236a28658a7a8a1022ac71
Artiko Wibowo <artikow@gmail.com>
2021-07-22 06:56:41 +0700

    nav bootstrap5 & font awesome

--- resources/stubs-bootstrap5/layout.blade.php
+++ resources/stubs-bootstrap5/layout.blade.php
@@ -9,5 +9,6 @@
         </div>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" data-auto-replace-svg="nest"></script>
     @yield('js')
 </html>
